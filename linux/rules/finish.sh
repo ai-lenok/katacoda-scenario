472e1e969508bcb6461e23d20b3dc7a15a197c6b
Anton Lenok <needfors@gmail.com>
2022-12-04 00:55:30 +0300

    Linux scenario

--- linux/rules/finish.sh
+++ linux/rules/finish.sh
@@ -2,6 +2,9 @@
 
 cat << EOF
 {
-    "script01": "OK"
+    "script01": "OK",
+    "script02": "FAIL: File or directory does not exist: _file_name_"
+    "script03": "FAIL: I found the wrong text: _word_"
+    "script04": "FAIL: The filename is incorrect: _file_name_"
 }
 EOF
