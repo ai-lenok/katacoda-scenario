613d9f6f5043fa6ad67fd57de34e24619f011c4a
Anton Lenok <needfors@gmail.com>
2022-12-04 21:07:44 +0300

    Linux scenario

--- linux/rules/finish.sh
+++ linux/rules/finish.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 
-import json
-
-# Data to be written
-check_result ={
-  "script01": "OK",
-  "script02": "FAIL: File or directory does not exist: _file_name_",
-  "script03": "FAIL: I found the wrong text: _word_",
-  "script04": "FAIL: The filename is incorrect: _file_name_",
-}
-
-json_object = json.dumps(check_result, indent = 4)
-print(json_object)
+python3 finish.py
