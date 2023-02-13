19c7f7b424f94dcba15111df324bc61fc1fbfd22
Anton Lenok <Lenok.A.Iv@sberbank.ru>
2023-02-13 19:17:57 +0300

    Add task docker-02

---
+++ cloud-docker-03/rules/finish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#if mvn verify &> /dev/null
+#then
+#  echo '{"result":"OK"}'
+#else
+#  echo '{"result":"FAIL: BUILD FAILURE"}'
+#fi
+
+echo '{"result":"Sorry, Homework does not finish yet"}'
