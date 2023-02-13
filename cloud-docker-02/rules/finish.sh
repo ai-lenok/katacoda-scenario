3851f412cb889275822e2b753bff5b239b3f9dee
Anton Lenok <Lenok.A.Iv@sberbank.ru>
2023-02-13 23:20:01 +0300

    Finish cloud-docker-02

--- cloud-docker-02/rules/finish.sh
+++ cloud-docker-02/rules/finish.sh
@@ -1,10 +1,114 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-#if mvn verify &> /dev/null
-#then
-#  echo '{"result":"OK"}'
-#else
-#  echo '{"result":"FAIL: BUILD FAILURE"}'
-#fi
+import json
+import subprocess
+import re
+import time
 
-echo '{"result":"Sorry, Homework does not finish yet"}'
+import requests
+
+
+class Checker:
+    def __init__(self):
+        self.stdout = ''
+        self.stderr = ''
+
+    def __run_script(self, command):
+        process = subprocess.run(command,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 universal_newlines=True)
+
+        stdout = process.stdout.strip()
+        stderr = process.stderr.strip()
+        return stdout, stderr
+
+    def check_image(self):
+        command = ["docker", "image", "ls"]
+        self.stdout, self.stderr = self.__run_script(command)
+
+        addressbook = re.compile(r'^\s*docker.io/dzx912/addressbook\s+1')
+        postgres = re.compile(r'^\s*docker.io/library/postgres')
+        find_addressbook = False
+        find_postgres = False
+        for line in self.stdout.split("\n"):
+            match_ab = re.match(addressbook, line)
+            match_postgres = re.match(postgres, line)
+            if match_ab:
+                find_addressbook = True
+            if match_postgres:
+                find_postgres = True
+
+        if find_addressbook and find_postgres:
+            return "OK"
+        else:
+            message = "FAIL: "
+            if not find_addressbook:
+                message += "Don't have addressbook image. "
+            if not find_postgres:
+                message += "Don't have postgres image. "
+            return message
+
+    def check_compose_ps(self):
+        command = ["docker-compose", "ps"]
+        self.stdout, self.stderr = self.__run_script(command)
+
+        count_containers_in_compose = len(self.stdout.split("\n")) - 1
+        if count_containers_in_compose == 2:
+            return "OK"
+        else:
+            return f"FAIL: Wrong count of compose containers: {count_containers_in_compose}. Need two."
+
+    def check_port(self):
+        try:
+            res = requests.get("http://localhost:8080/api/v1/addressbooks")
+            if res.status_code == 200:
+                return "OK"
+            else:
+                return "FAIL: Port does not open"
+        except:
+            return "FAIL: Port does not open"
+
+    def check_db(self):
+        try:
+            rows_start = self.__count_rows()
+            need_rows_after_put = rows_start + 1
+
+            self.__add_row()
+            rows_added = self.__count_rows()
+
+            self.__restart_compose()
+            time.sleep(5)
+
+            rows_end = self.__count_rows()
+
+            if need_rows_after_put == rows_added and \
+                    rows_added == rows_end:
+                return "OK"
+            else:
+                return "FAIL: Storage check failed"
+        except Exception as e:
+            return f"FAIL: {e}"
+
+    def __add_row(self):
+        data = {"firstName": "Ivan", "lastName": "Ivanov", "phone": "+79999999999", "birthday": "2000-01-01"}
+        requests.post("http://localhost:8080/api/v1/addressbook", json=data)
+
+    def __count_rows(self):
+        addressbooks = requests.get("http://localhost:8080/api/v1/addressbooks")
+        return len(addressbooks.json())
+
+    def __restart_compose(self):
+        self.__run_script(["docker-compose", "down"])
+        self.__run_script(["docker-compose", "up", "-d"])
+
+
+if __name__ == '__main__':
+    checker = Checker()
+    check_result = {}
+    check_result["docker image"] = checker.check_image()
+    check_result["docker-compose ps"] = checker.check_compose_ps()
+    check_result["open port"] = checker.check_port()
+    check_result["storage"] = checker.check_db()
+    json_object = json.dumps(check_result, indent=4)
+    print(json_object)
