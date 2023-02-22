2e2044aad4fa074eb5917440200cfce205873eda
Anton Lenok <Lenok.A.Iv@sberbank.ru>
2023-02-22 12:14:59 +0300

    Add checker to exercise OS Pod 03

--- cloud-os-pod-03/rules/finish.sh
+++ cloud-os-pod-03/rules/finish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-#printf '{"Message": "FAIL: %s"}' $(oc get pods)
-echo '{' '"Message"' ':' '"FAIL:"' '"' $(date) '"' '}'
+printf '{"Message": "FAIL": "%s"}' $(oc get pods)
+#printf '{'Message': "FAIL: '%s'}" $(oc get pods)
+#echo '{' '"Message"' ':' '"FAIL:"' '"' $(date) '"' '}'
