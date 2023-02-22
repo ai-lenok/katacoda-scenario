3b736fdc46d6e9b2bcf82a879680f023df1b1ab7
Anton Lenok <Lenok.A.Iv@sberbank.ru>
2023-02-22 12:59:14 +0300

    Add checker to exercise OS Pod 04

--- cloud-os-pod-04/rules/finish.sh
+++ cloud-os-pod-04/rules/finish.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+PS1=1
+source /root/.bashrc
 
-printf "{\"Message\": \"FAIL: $(oc get pods)\"}"
+ADDRESS_BOOK_POD=$(oc get pods -o name | grep addressbook | wc -l)
+
+#printf "{\"Message\": \"FAIL: $(oc get pods)\"}"
+
+jq --null-input \
+--arg ADDRESS_BOOK_POD "$ADDRESS_BOOK_POD" \
+--arg SIDECAR_LOG "$SIDECAR_LOG" \
+--arg EGRESS_LOG "$EGRESS_LOG" \
+--arg GW "$GW" \
+--arg VS "$VS" \
+--arg SE "$SE" \
+'{
+    "pod": $ADDRESS_BOOK_POD
+}'
