12f14d9404cecf408c97019aee9f242cd65d5498
Anton Lenok <needfors@gmail.com>
2023-07-25 21:26:26 +0300

    Change check script in cloud-brief-02

--- cloud-brief-02/rules/finish.sh
+++ cloud-brief-02/rules/finish.sh
@@ -1,8 +1,49 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-if mvn verify &> /dev/null
-then
-  echo '{"result":"OK"}'
-else
-  echo '{"result":"FAIL: BUILD FAILURE"}'
-fi
+import json
+import pathlib
+import subprocess
+
+import requests
+
+
+class Checker:
+    def __init__(self):
+        self.stdout = ''
+        self.stderr = ''
+        self.path_to_jar_file = 'target/demo-1.jar'
+        self.url_test = "http://localhost:8080/api/v1/test"
+
+    def __run_script(self, command):
+        process = subprocess.run(command,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 universal_newlines=True)
+
+        stdout = process.stdout.strip()
+        stderr = process.stderr.strip()
+        return stdout, stderr
+
+    def check_file_exists(self):
+        if pathlib.Path(self.path_to_jar_file).exists():
+            return "OK"
+        else:
+            return f'FAIL: Файл не существует: "{self.path_to_jar_file}"'
+
+    def check_port(self):
+        try:
+            res = requests.get(self.url_test)
+            if res.status_code == 200:
+                return "OK"
+            else:
+                return "FAIL: Порт 8080 закрыт"
+        except:
+            return "FAIL: Порт 8080 закрыт"
+
+
+if __name__ == '__main__':
+    checker = Checker()
+    check_result = {"File exists": checker.check_file_exists(),
+                    "Check port": checker.check_port()}
+    json_object = json.dumps(check_result)
+    print(json_object)
