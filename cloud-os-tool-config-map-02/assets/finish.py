bf68268a789a74b3a8534612da2bdf22ee43ceb8
Anton Lenok <needfors@gmail.com>
2023-07-30 23:05:57 +0300

    Fix cloud-os-tool-05

--- cloud-os-tool-config-map-02/assets/finish.py
+++ cloud-os-tool-config-map-02/assets/finish.py
@@ -33,17 +33,17 @@ class Checker:
             info = json.loads(self.stdout)
             data = info["data"]
             if self.key_name_config_map_expect not in data:
-                return f"FAIL: В ConfigMap {self.config_map_expect} отсутствует поле {self.key_name_config_map_expect}"
+                return f'FAIL: В ConfigMap {self.config_map_expect} отсутствует поле "{self.key_name_config_map_expect}".'
             config_map_data_actual = yaml.safe_load(data["application.yaml"])
             if "app" not in config_map_data_actual:
-                return f'FAIL: В ConfigMap {self.config_map_expect} -> {self.key_name_config_map_expect} ' \
+                return f'FAIL: В ConfigMap "{self.config_map_expect}" -> "{self.key_name_config_map_expect}" ' \
                        f'отсутствует поле "app".'
 
             result_check_configs = \
                 self.compare_dict(config_map_data_actual["app"],
                                   self.application_yaml_app_expect,
-                                  f"FAIL: В ConfigMap {self.config_map_expect} "
-                                  f'-> {self.key_name_config_map_expect} -> "app" неправильные поля. ')
+                                  f'FAIL: В ConfigMap "{self.config_map_expect}" '
+                                  f'-> "{self.key_name_config_map_expect}" -> "app" неправильные поля. ')
 
             if result_check_configs:
                 return result_check_configs
@@ -58,9 +58,9 @@ class Checker:
             return ""
 
         fail_msg = prefix_msg
-        for key, value in dict(diff):
+        for key in diff:
             if key in actual:
-                fail_msg += f'Неправильное значение в "{key}": "{actual[key]}"'
+                fail_msg += f'Неправильное значение в "{key}": "{actual[key]}", должен быть "{expect[key]}"'
             else:
                 fail_msg += f' Отсутствует "{key}".'
         return fail_msg
