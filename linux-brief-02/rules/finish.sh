0b97f2cbf1b915c0d47c1fe8f2ba5f65e8678aed
Anton Lenok <needfors@gmail.com>
2024-02-04 20:44:56 +0300

    Improve brief

--- linux-brief-02/rules/finish.sh
+++ linux-brief-02/rules/finish.sh
@@ -2,45 +2,21 @@
 
 import json
 import pathlib
-import subprocess
-
 
 class Checker:
     def __init__(self):
         self.stdout = ''
         self.stderr = ''
-        self.path = f'/home/ubuntu/script.sh'
-
-    def __run_script(self):
-        subprocess.run(['chmod', '+x', self.path])
-        process = subprocess.run([self.path],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
-                                 universal_newlines=True)
-
-        stdout = process.stdout.strip()
-        stderr = process.stderr.strip()
-        return stdout, stderr
+        self.path = f'/home/ubuntu/file.txt'
 
     def check(self):
         if not pathlib.Path(self.path).is_file():
-            return f'FAIL: {self.path} не существует'
+            return f'FAIL: Файл "{self.path}" не существует'
 
-        self.stdout, self.stderr = self.__run_script()
-
-        return self.check_script_00_print_hello_world()
-
-    def check_script_00_print_hello_world(self):
-        if not self.stdout:
-            return f"FAIL: Скрипт ничего не вывел"
-
-        if self.stdout == "Hello world":
-            return "OK"
-        else:
-            return f"FAIL: Неправильный ответ: {self.stdout}"
+        return "OK"
 
 
 if __name__ == '__main__':
-    check_result = {"echo": Checker().check()}
+    check_result = {"create": Checker().check()}
     json_object = json.dumps(check_result)
     print(json_object)
