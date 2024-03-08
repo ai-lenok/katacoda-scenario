9ba8be501a623fcc34eeb604c494117aff4e0481
Anton Lenok <needfors@gmail.com>
2024-03-08 20:45:45 +0300

    Fix linux-navigation

--- linux-navigation-02/rules/finish.sh
+++ linux-navigation-02/rules/finish.sh
@@ -11,7 +11,7 @@ class Checker:
         self.stdout = ''
         self.stderr = ''
         self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
-        self.reference_output = kwargs.get("reference_output", '/root')
+        self.reference_output = kwargs.get("reference_output", '/home/ubuntu')
         self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
         os.chdir(self.current_dir)
 
