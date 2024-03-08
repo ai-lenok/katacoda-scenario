6802954e99dcf5ebb5c20d150ada7574a8fe1dcb
Anton Lenok <needfors@gmail.com>
2024-03-08 23:18:14 +0300

    Add linux-navigation 08

--- linux-navigation-08/rules/finish.sh
+++ linux-navigation-08/rules/finish.sh
@@ -2,49 +2,24 @@
 
 import json
 import os
-import subprocess
-from pathlib import Path
-
+import pathlib
 
 class Checker:
     def __init__(self, **kwargs):
         self.stdout = ''
         self.stderr = ''
-        self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
-        self.reference_output = kwargs.get("reference_output", 'Hello world')
+        self.path = kwargs.get("check_path_exists", '/home/ubuntu/my_dir')
         self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
         os.chdir(self.current_dir)
 
-    def __run_script(self):
-        subprocess.run(['chmod', '+x', self.checking_script])
-        process = subprocess.run([self.checking_script],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
-                                 universal_newlines=True)
-
-        stdout = process.stdout.strip()
-        stderr = process.stderr.strip()
-        return stdout, stderr
-
     def check(self):
-        if not Path(self.checking_script).is_file():
-            return f'FAIL: {self.checking_script} не существует'
-
-        self.stdout, self.stderr = self.__run_script()
-
-        return self.check_script_output()
-
-    def check_script_output(self):
-        if not self.stdout:
-            return f"FAIL: Скрипт ничего не вывел"
-
-        if self.stdout == self.reference_output:
-            return "OK"
+        if pathlib.Path(self.path).exists():
+            return f'FAIL: Каталог "{self.path}" существует'
         else:
-            return f'FAIL: Неправильный ответ: "{self.stdout}"'
+            return "OK"
 
 
 if __name__ == '__main__':
-    check_result = {"echo": Checker().check()}
+    check_result = {"create": Checker().check()}
     json_object = json.dumps(check_result)
     print(json_object)
---
+++ linux-navigation-08/test/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf *
+mkdir my_dir
+touch my_dir/file
