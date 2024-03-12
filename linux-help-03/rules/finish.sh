ee33fe1cbf712b26ba9d937b5b24b89df65eb52e
Anton Lenok <needfors@gmail.com>
2024-03-12 22:09:21 +0300

    Add linux-help-03

--- linux-help-03/rules/finish.sh
+++ linux-help-03/rules/finish.sh
@@ -2,6 +2,7 @@
 
 import json
 import os
+import re
 import subprocess
 from pathlib import Path
 
@@ -11,7 +12,7 @@ class Checker:
         self.stdout = ''
         self.stderr = ''
         self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
-        self.reference_output = kwargs.get("reference_output", 'Hello world')
+        self.reference_output = kwargs.get("reference_output", 'java (1)             - launch a Java application')
         self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
         os.chdir(self.current_dir)
 
