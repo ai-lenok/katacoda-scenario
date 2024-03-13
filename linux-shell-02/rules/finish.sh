af8020dfdd3ca723fc1f9c4c7e03fe4c4836378d
Anton Lenok <needfors@gmail.com>
2024-03-13 12:22:09 +0300

    Add linux-shell-02

--- linux-shell-02/rules/finish.sh
+++ linux-shell-02/rules/finish.sh
@@ -11,37 +11,56 @@ class Checker:
         self.stdout = ''
         self.stderr = ''
         self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
-        self.reference_output = kwargs.get("reference_output", 'Hello world')
+        self.reference_success = kwargs.get("reference_success", '0')
+        self.reference_fail = kwargs.get("reference_fail", '1')
+
+        self.command_success = kwargs.get("command_success", 'bash -c "exit 0"; \n')
+        self.command_fail = kwargs.get("command_fail", 'bash -c "exit 1"; \n')
+
         self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
         os.chdir(self.current_dir)
 
-    def __run_script(self):
-        subprocess.run(['chmod', '+x', self.checking_script])
-        process = subprocess.run([self.checking_script],
+    def __run_command(self, command: str):
+        new_command = command
+        with open(self.checking_script) as file:
+            new_command += file.read()
+        process = subprocess.run([new_command],
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
-                                 universal_newlines=True)
+                                 universal_newlines=True,
+                                 shell=True,
+                                 executable="/bin/bash")
 
-        stdout = process.stdout.strip()
-        stderr = process.stderr.strip()
-        return stdout, stderr
+        self.stdout = process.stdout.strip()
+        self.stderr = process.stderr.strip()
 
     def check(self):
         if not Path(self.checking_script).is_file():
             return f'FAIL: {self.checking_script} не существует'
 
-        self.stdout, self.stderr = self.__run_script()
+        self.__run_command(self.command_success)
 
-        return self.check_script_output()
-
-    def check_script_output(self):
         if not self.stdout:
             return f"FAIL: Скрипт ничего не вывел"
 
-        if self.stdout == self.reference_output:
-            return "OK"
-        else:
+        if self.reference_success != self.stdout and self.reference_fail != self.stdout:
             return f'FAIL: Неправильный ответ: "{self.stdout}"'
+
+        if self.reference_success != self.stdout:
+            return 'FAIL: Скрипт вернул ошибку, хотя был успех'
+
+        self.__run_command(self.command_fail)
+
+        if not self.stdout:
+            return f"FAIL: Скрипт ничего не вывел"
+
+        if self.reference_success != self.stdout and self.reference_fail != self.stdout:
+            return f'FAIL: Неправильный ответ: "{self.stdout}"'
+
+        if self.reference_fail != self.stdout:
+            return 'FAIL: Скрипт вернул успех, хотя была ошибка'
+
+        return "OK"
 
 
 if __name__ == '__main__':
