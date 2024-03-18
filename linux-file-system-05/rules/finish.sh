4df78b06fad57de8a3dbf13388f5ee8027c35a13
Anton Lenok <needfors@gmail.com>
2024-03-18 12:40:26 +0300

    Add linux-file-system-05

--- linux-file-system-05/rules/finish.sh
+++ linux-file-system-05/rules/finish.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import json
-import os
-import subprocess
 from pathlib import Path
 
 
@@ -10,41 +8,23 @@ class Checker:
     def __init__(self, **kwargs):
         self.stdout = ''
         self.stderr = ''
-        self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
-        self.reference_output = kwargs.get("reference_output", 'Hello world')
-        self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
-        os.chdir(self.current_dir)
-
-    def __run_script(self):
-        subprocess.run(['chmod', '+x', self.checking_script])
-        process = subprocess.run([self.checking_script],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
-                                 universal_newlines=True)
-
-        stdout = process.stdout.strip()
-        stderr = process.stderr.strip()
-        return stdout, stderr
+        self.path_source = kwargs.get("path_source", '/home/ubuntu/file.txt')
+        self.path_destination = kwargs.get("path_destination", '/home/ubuntu/link-file.txt')
 
     def check(self):
-        if not Path(self.checking_script).is_file():
-            return f'FAIL: {self.checking_script} не существует'
+        if not Path(self.path_source).is_file():
+            return f'FAIL: Файл "{self.path_source}" исчез'
 
-        self.stdout, self.stderr = self.__run_script()
+        if not Path(self.path_destination).is_file():
+            return f'FAIL: Файл "{self.path_destination}" не существует'
 
-        return self.check_script_output()
+        if not Path(self.path_destination).is_symlink():
+            return f'FAIL: Файл "{self.path_destination}" не является ссылкой'
 
-    def check_script_output(self):
-        if not self.stdout:
-            return f"FAIL: Скрипт ничего не вывел"
-
-        if self.stdout == self.reference_output:
-            return "OK"
-        else:
-            return f'FAIL: Неправильный ответ: "{self.stdout}"'
+        return "OK"
 
 
 if __name__ == '__main__':
-    check_result = {"echo": Checker().check()}
+    check_result = {"create": Checker().check()}
     json_object = json.dumps(check_result)
     print(json_object)
