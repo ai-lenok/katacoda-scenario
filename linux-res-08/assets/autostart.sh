84bfe43129559ba938800cfa86e20c8cce837b66
Anton Lenok <needfors@gmail.com>
2024-03-22 20:08:25 +0300

    Add linux-res-08

--- linux-res-08/assets/autostart.sh
+++ linux-res-08/assets/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-runuser -u ubuntu -- python3 -m http.server
+runuser -u ubuntu -- python3 -m http.server -d /home/ubuntu/web
 
 DONE_FILE=/usr/local/etc/autostart.sh.done
 touch $DONE_FILE
