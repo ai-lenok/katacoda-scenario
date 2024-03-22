4ee9d17d279c62cafc6ebbf0e3415ccc4954bdbb
Anton Lenok <needfors@gmail.com>
2024-03-22 20:56:05 +0300

    Add linux-res-08

--- linux-res-08/rules/finish.sh
+++ linux-res-08/rules/finish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
+import requests
+
 sys.path.insert(1, '/usr/local/lib/')
 
 from ExerciseTester.tester import Tester
@@ -8,9 +10,8 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        if "reference_output" not in kwargs:
-            kwargs["reference_output"] = "Hello world"
         super().__init__(**kwargs)
+        self.address = kwargs.get("address", 'http://localhost:8000')
 
     def check(self) -> str:
         return (self
@@ -18,6 +19,13 @@ class Checker(Tester):
                 .do(self.not_empty)
                 .do(self.compare_text)
                 .finish())
+
+    def request(self):
+        try:
+            requests.get(self.address)
+            return self.fail("Сервер работает")
+        except requests.exceptions.ConnectionError:
+            return self.ok("Сервер выключен")
 
 
 if __name__ == '__main__':
