62bfc940bc6281c4885894d547a72e28e836686c
Anton Lenok <needfors@gmail.com>
2024-03-22 23:53:28 +0300

    Add linux-res-09

---
+++ linux-res-09/assets/prime.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import sys
+
+
+def nth_prime_number(n):
+    # initial prime number list
+    prime_list = [2]
+    # first number to test if prime
+    num = 3
+    # keep generating primes until we get to the nth one
+    while len(prime_list) < n:
+
+        # check if num is divisible by any prime before it
+        for p in prime_list:
+            # if there is no remainder dividing the number
+            # then the number is not a prime
+            if num % p == 0:
+                # break to stop testing more numbers, we know it's not a prime
+                break
+
+        # if it is a prime, then add it to the list
+        # after a for loop, else runs if the "break" command has not been given
+        else:
+            # append to prime list
+            prime_list.append(num)
+
+        # same optimization you had, don't check even numbers
+        num += 2
+
+    # return the last prime number generated
+    return prime_list[-1]
+
+
+if __name__ == "__main__":
+    order = int(sys.argv[1])
+    prime_number = nth_prime_number(order)
+    print(f"{order}th prime number = {prime_number}")
