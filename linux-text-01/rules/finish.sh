60397df56e16bf846217933b9b551b7a976d640d
Anton Lenok <needfors@gmail.com>
2024-03-23 16:10:38 +0300

    Add linux-text-01

--- linux-text-01/rules/finish.sh
+++ linux-text-01/rules/finish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from pathlib import Path
 
 sys.path.insert(1, '/usr/local/lib/')
 
@@ -8,15 +9,32 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        kwargs.setdefault("reference_reading_file", "file.txt")
+        kwargs.setdefault("is_message_success_from_stdout", True)
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
                 .do(self.run)
+                .do(self.reading_file_exists)
+                .do(self.stop_words)
                 .do(self.not_empty)
-                .do(self.compare_text)
+                .do(self.compare_part_file)
                 .finish())
+
+    def reading_file_exists(self):
+        if not Path(self.params["reference_reading_file"]).is_file():
+            return self.fail(f'{self.params["reference_reading_file"]} не существует')
+        return self
+
+    def compare_part_file(self):
+        with open(self.params["reference_reading_file"], "r") as file:
+            text = file.read().split("\n")
+            output_lines = self.stdout.split("\n")
+            if text[:5] == output_lines:
+                return self.ok()
+
+        return self.fail(f"Неправильный ответ:\n\n{self.stdout}")
 
 
 if __name__ == '__main__':
---
+++ linux-text-01/test/init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rm -rf *
+cat >file.txt <<EOL
+line 1
+line 2
+line 3
+line 4
+line 5
+line 6
+line 7
+line 8
+line 9
+line 10
+line 12
+line 13
+line 14
+line 15
+line 16
+EOL
