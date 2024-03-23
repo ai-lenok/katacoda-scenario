6e2d4c70e6ffe291f0c9838746d51291aaea6bec
Anton Lenok <needfors@gmail.com>
2024-03-23 16:44:38 +0300

    Fix linux-text-01

--- linux-text-01/rules/finish.sh
+++ linux-text-01/rules/finish.sh
@@ -10,6 +10,7 @@ from ExerciseTester.tester import Tester
 class Checker(Tester):
     def __init__(self, **kwargs):
         kwargs.setdefault("reference_reading_file", "file.txt")
+        kwargs.setdefault("count_file_lines", 4)
         kwargs.setdefault("is_message_success_from_stdout", True)
         super().__init__(**kwargs)
 
@@ -19,6 +20,7 @@ class Checker(Tester):
                 .do(self.reading_file_exists)
                 .do(self.stop_words)
                 .do(self.not_empty)
+                .do(self.compare_wrong_part_file)
                 .do(self.compare_part_file)
                 .finish())
 
@@ -27,11 +29,22 @@ class Checker(Tester):
             return self.fail(f'{self.params["reference_reading_file"]} не существует')
         return self
 
+    def compare_wrong_part_file(self):
+        with open(self.params["reference_reading_file"], "r") as file:
+            text = file.read().split("\n")
+            output_lines = self.stdout.split("\n")
+            count_file_lines = self.params["count_file_lines"]
+            if text[:10] == output_lines:
+                return self.fail(f"Нужно вывести {count_file_lines} строк(и)")
+
+        return self
+
     def compare_part_file(self):
         with open(self.params["reference_reading_file"], "r") as file:
             text = file.read().split("\n")
             output_lines = self.stdout.split("\n")
-            if text[:5] == output_lines:
+            count_file_lines = self.params["count_file_lines"]
+            if text[:count_file_lines] == output_lines:
                 return self.ok()
 
         return self.fail(f"Неправильный ответ:\n\n{self.stdout}")
