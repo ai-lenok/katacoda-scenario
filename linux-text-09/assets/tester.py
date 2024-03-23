ba409af462ce62c7f22781d09400c78b2cb7a1f6
Anton Lenok <needfors@gmail.com>
2024-03-23 15:07:54 +0300

    Update tester.py

--- linux-text-09/assets/tester.py
+++ linux-text-09/assets/tester.py
@@ -14,23 +14,24 @@ class TestStatus(enum.Enum):
 
 class Tester:
     def __init__(self, **kwargs):
+        self.params = kwargs
         self.status = TestStatus.not_finish
         self.message = ""
 
         self.stdout = ''
         self.stderr = ''
 
-        self.is_message_success_from_stdout = kwargs.get("is_message_success_from_stdout", False)
-        self.message_success = kwargs.get("message_success", "")
-        self.check_name = kwargs.get("check_name", "check")
+        self.params.setdefault("is_message_success_from_stdout", False)
+        self.params.setdefault("message_success", "")
+        self.params.setdefault("check_name", "check")
 
-        self.reference_output = kwargs.get("reference_output", "")
-        self.reference_pattern = kwargs.get("reference_pattern", "")
+        self.params.setdefault("reference_output", "")
+        self.params.setdefault("reference_pattern", "")
 
-        self.checking_script = kwargs.get("checking_script", '/home/ubuntu/script.sh')
+        self.params.setdefault("checking_script", '/home/ubuntu/script.sh')
 
-        self.current_dir = kwargs.get("current_dir", '/home/ubuntu')
-        os.chdir(self.current_dir)
+        self.params.setdefault("current_dir", '/home/ubuntu')
+        os.chdir(self.params["current_dir"])
 
     def do(self, func):
         if self.status == TestStatus.not_finish:
@@ -42,10 +43,10 @@ class Tester:
         self.status = TestStatus.ok
         if message:
             self.message = message
-        elif self.is_message_success_from_stdout:
+        elif self.params["is_message_success_from_stdout"]:
             self.message = self.stdout
         else:
-            self.message = self.message_success
+            self.message = self.params["message_success"]
         return self
 
     def fail(self, message=""):
@@ -54,7 +55,7 @@ class Tester:
         return self
 
     def finish_json(self) -> str:
-        result = {self.check_name: self.check()}
+        result = {self.params["check_name"]: self.check()}
         return json.dumps(result, ensure_ascii=False)
 
     def check(self):
@@ -69,8 +70,8 @@ class Tester:
             return "FAIL: Автотест не смог проверить результат"
 
     def run_script(self):
-        subprocess.run(['chmod', '+x', self.checking_script])
-        process = subprocess.run([self.checking_script],
+        subprocess.run(['chmod', '+x', self.params["checking_script"]])
+        process = subprocess.run([self.params["checking_script"]],
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
                                  universal_newlines=True)
@@ -80,8 +81,8 @@ class Tester:
         return self
 
     def run(self):
-        if not Path(self.checking_script).is_file():
-            return self.fail(f'{self.checking_script} не существует')
+        if not Path(self.params["checking_script"]).is_file():
+            return self.fail(f'{self.params["checking_script"]} не существует')
 
         return self.run_script()
 
@@ -91,13 +92,13 @@ class Tester:
         return self
 
     def compare_text(self):
-        if self.stdout == self.reference_output:
+        if self.stdout == self.params["reference_output"]:
             return self.ok()
         else:
             return self.fail(f"Неправильный ответ:\n\n{self.stdout}")
 
     def compare_regex(self):
-        if re.match(self.reference_pattern, self.stdout):
+        if re.match(self.params["reference_pattern"], self.stdout):
             return self.ok()
         else:
             return self.fail(f"Неправильный ответ:\n\n{self.stdout}")
