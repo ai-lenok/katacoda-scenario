edd5dab2666ab6de4ad6d2623c275644d48030f4
Anton Lenok <needfors@gmail.com>
2024-03-24 20:28:34 +0300

    Add linux-net-01

--- linux-net-01/rules/finish.sh
+++ linux-net-01/rules/finish.sh
@@ -8,13 +8,15 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        kwargs.setdefault("reference_command", f'curl localhost:8000')
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
                 .do(self.run)
+                .do(self.stop_words)
                 .do(self.not_empty)
+                .do(self.reference_command)
                 .do(self.compare_text)
                 .finish())
 
