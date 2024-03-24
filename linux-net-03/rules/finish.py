1470aa749cd1c527fbecf3ef80bf2e57bb5eb877
Anton Lenok <needfors@gmail.com>
2024-03-24 22:24:08 +0300

    Add linux-net-03

--- linux-net-03/rules/finish.py
+++ linux-net-03/rules/finish.py
@@ -8,13 +8,14 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        kwargs.setdefault("reference_command", f'nc -zv localhost:8000')
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
                 .do(self.run)
                 .do(self.not_empty)
+                .do(self.reference_command)
                 .do(self.compare_text)
                 .finish())
 
