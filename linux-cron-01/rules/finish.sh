5f3b466bac4ca03c7805a2272d6133368826e2f3
Anton Lenok <needfors@gmail.com>
2024-03-25 19:21:57 +0300

    Add linux-cron-01

--- linux-cron-01/rules/finish.sh
+++ linux-cron-01/rules/finish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 import sys
 
 sys.path.insert(1, '/usr/local/lib/')
@@ -8,15 +9,23 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        kwargs.setdefault("reference_command", "crontab -l")
+        kwargs.setdefault("reference_pattern", r'^(\*\s+){5}')
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
-                .do(self.run)
-                .do(self.not_empty)
-                .do(self.compare_text)
+                .do(self.reference_command)
+                .do(self.cron_condition_analysis)
                 .finish())
+
+    def cron_condition_analysis(self):
+        lines = self.params["reference_output"].split('\n')
+
+        for line in lines:
+            if re.match(self.params["reference_pattern"], line):
+                return self.ok()
+        return self.fail("Не смог найти запланированную задачу")
 
 
 if __name__ == '__main__':
