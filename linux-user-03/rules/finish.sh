9c1cdaad92639d430ed26fbd73636d6197eb08dd
Anton Lenok <needfors@gmail.com>
2024-03-26 14:54:11 +0300

    Add linux-user-03

--- linux-user-03/rules/finish.sh
+++ linux-user-03/rules/finish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 import sys
 
 sys.path.insert(1, '/usr/local/lib/')
@@ -8,15 +9,25 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        # docker:x:130:anton,user
+        kwargs.setdefault("adding_user", 'user')
+        kwargs.setdefault("adding_group", 'docker')
+        kwargs.setdefault("reference_command", 'cat /etc/group')
+        kwargs.setdefault("reference_pattern",
+                          f'{kwargs["adding_group"]}:x:\d+:[\w,]*{kwargs["adding_user"]}[\w,]*')
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
-                .do(self.run)
-                .do(self.not_empty)
-                .do(self.compare_text)
+                .do(self.reference_command)
+                .do(self.compare_regex)
                 .finish())
+
+    def compare_regex(self):
+        if re.match(self.params["reference_pattern"], self.params["reference_output"]):
+            return self.ok()
+        else:
+            return self.fail(f'Пользователя {self.params["adding_user"]} нет в группе {self.params["adding_user"]}')
 
 
 if __name__ == '__main__':
