2d93e44d547dce158efc6564c777bbad03328dc3
Anton Lenok <needfors@gmail.com>
2024-03-26 13:59:54 +0300

    Add linux-user-01

--- linux-user-01/rules/finish.sh
+++ linux-user-01/rules/finish.sh
@@ -8,13 +8,16 @@ from ExerciseTester.tester import Tester
 
 class Checker(Tester):
     def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
+        kwargs.setdefault("reference_command", 'cat /etc/passwd')
+        kwargs.setdefault("is_message_success_from_stdout", True)
         super().__init__(**kwargs)
 
     def check(self) -> str:
         return (self
                 .do(self.run)
+                .do(self.stop_words)
                 .do(self.not_empty)
+                .do(self.reference_command)
                 .do(self.compare_text)
                 .finish())
 
--- linux-user-01/step-1.md
+++ linux-user-01/step-1.md
@@ -1,12 +1,20 @@
-
+Выведите список всех пользователей в системе
 
 ## Детали
 
 Откройте файл `script.sh`{{open}}
 
-Напишите команду, которая
+Напишите команду, которая отобразит файл, содержащий список всех пользователей в системе
 
+Пример вывода:
 
+```text
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin
+user:x:1000:1000:Regular user:/home/user:/bin/bash
+```
 
 ## Способ проверки
 
