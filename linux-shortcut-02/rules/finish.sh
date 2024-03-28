7563690944129f5d37273285c385062f3ca06292
Anton Lenok <needfors@gmail.com>
2024-03-28 19:54:35 +0300

    Fix linux-user-04

--- linux-shortcut-02/rules/finish.sh
+++ linux-shortcut-02/rules/finish.sh
@@ -1,23 +1,3 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/env bash
 
-sys.path.insert(1, '/usr/local/lib/')
-
-from ExerciseTester.tester import Tester
-
-
-class Checker(Tester):
-    def __init__(self, **kwargs):
-        kwargs.setdefault("reference_output", "Hello world")
-        super().__init__(**kwargs)
-
-    def check(self) -> str:
-        return (self
-                .do(self.run)
-                .do(self.not_empty)
-                .do(self.compare_text)
-                .finish())
-
-
-if __name__ == '__main__':
-    print(Checker().finish_json())
+echo '{"OK":"OK"}'
